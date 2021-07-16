91dfc6efe26e5290360ecab3cb2b5d856d69b478
dsdannynikravesh <danny@delicious-simplicity.com>
2021-07-15 18:35:18 -0700

    Revert "Test replace"
    This reverts commit ca66c04b39ad4617db53e8c686daa6888794cd6e.

--- src/index.tsx
+++ src/index.tsx
@@ -238,7 +238,7 @@ const BetterSlugs = ({ sdk }: BetterSlugsProps) => {
     }
 
     sdk.entry.fields[sdk.field.id].setValue(
-      slugParts.join('/').replace('//', '/pooper').replace(/\/$/, 'pooper'),
+      slugParts.join('/').replace('//', '/').replace(/\/$/, ''),
       locale
     );
   };
